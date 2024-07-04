90b12a49d0c5ff029242e9c6142b07edc89b625b
bada <346025425@qq.com>
2024-07-05 00:53:03 +0800

    Update run_aleo_apool_gpu.sh

--- run_aleo_apool_gpu.sh
+++ run_aleo_apool_gpu.sh
@@ -15,5 +15,5 @@ if ps aux | grep 'apoolminer' | grep -q 'apool.io'; then
     echo "ApoolMiner already running."
     exit 1
 else
-    nohup ./apoolminer --pool aleo1.hk.apool.io:9090 --account $account --worker $worker -A aleo -g 0 >> aleo.log 2>&1 &
+    nohup ./apoolminer --pool aleo1.hk.apool.io:9090 --account $account --worker $worker -A aleo -g 0 &
 fi
