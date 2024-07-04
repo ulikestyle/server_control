bec987c2b34645821fdabcfd0b1e8f86af709ed4
bada <346025425@qq.com>
2024-07-05 00:41:30 +0800

    Update run_aleo_apool_gpu.sh

--- run_aleo_apool_gpu.sh
+++ run_aleo_apool_gpu.sh
@@ -8,14 +8,14 @@ sudo rm -rf run_aleo_apool_gpu*
 sudo rm -rf aleo_gpu
 sudo mkdir aleo_gpu
 cd aleo_gpu
-sudo touch aleo.log
 
 sudo wget https://github.com/apool-io/apoolminer/releases/download/v1.6.6/apoolminer_linux_v1.6.6.tar
 sudo tar -xvf apoolminer_linux_v1.6.6.tar
+sudo touch aleo.log
 
 if ps aux | grep 'apoolminer' | grep -q 'apool.io'; then
     echo "ApoolMiner already running."
     exit 1
 else
-    sudo nohup  ./apoolminer --pool aleo1.hk.apool.io:9090 --account $account --worker $worker -A aleo -g 0 >> aleo.log 2>&1 &
+    sudo nohup ./apoolminer --pool aleo1.hk.apool.io:9090 --account $account --worker $worker -A aleo -g 0 >> aleo.log 2>&1 &
 fi
