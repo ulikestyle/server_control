66bb996d32c71fec63e51af041c17a4db0361d74
bada <346025425@qq.com>
2024-08-04 23:34:28 +0800

    Update run_qubic_apool_gpu.sh

--- run_qubic_apool_gpu.sh
+++ run_qubic_apool_gpu.sh
@@ -8,8 +8,8 @@ sudo rm -rf aleo_gpu
 sudo mkdir aleo_gpu
 cd aleo_gpu
 
-sudo wget https://github.com/apool-io/apoolminer/releases/download/v1.6.10/apoolminer_linux_v1.6.10.tar
-sudo tar -xvf apoolminer_linux_v1.6.10.tar
+sudo wget https://github.com/apool-io/apoolminer/releases/download/v1.6.15/apoolminer_linux_v1.6.15.tar
+sudo tar -xvf apoolminer_linux_v1.6.15.tar
 
 if ps aux | grep 'apoolminer' | grep -q 'apool.io'; then
     echo "ApoolMiner already running."
